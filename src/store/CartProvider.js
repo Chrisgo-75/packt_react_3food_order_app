92be15950937e47a94d17bf67e9eb909f7d5d99d
Chris Arndt <christopher.arndt@wisc.edu>
2022-07-26 09:40:15 -0500

    Adding a cart reducer.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,49 @@
+// Due to complex State logic will use useReducer instead of useState.
+import { useReducer } from "react";
+
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+};
 
-  const removeItemFromCartHandler = (id) => {};
+// Creating cartReducer OUTSIDE of the CartProvider component b/c
+// a. cartReducer doesn't need anything from within CartProvider component.
+// b. and cartReducer shouldn't be re-created all the time when CartProvider
+//    component is re-evaluated.
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+    // concat (JS) adds new item to an Array and returns a new array.
+    // vs push (JS) edits existing array.
+    // So you want to create a new array instead of editing the old array in memory.
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    };
+  }
+  return (defaultCartState);
+};
+
+
+const CartProvider = (props) => {
+  // Call React Hook "useReducer()"
+  // useReducer returns an Array with 2 elements.
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({type: 'ADD', item: item});
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({type: 'REMOVE', id: id});
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     AddItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler
   }
